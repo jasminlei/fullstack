a1325bd7616ffd8367b9cc5414cb417c0f83f09a
jasminlei <jasminleinonen@hotmail.com>
2024-12-27 12:33:42 +0200

    teht 2.1

---
+++ osa2/kurssitiedot/src/App.jsx
@@ -0,0 +1,65 @@
+const Course = ({ course }) => {
+  const Header = ({ course }) => {
+    return <h1>{course.name}</h1>;
+  };
+
+  const Part = ({ part }) => {
+    return (
+      <div>
+        <p>
+          {part.name} {part.exercises}
+        </p>
+      </div>
+    );
+  };
+
+  const Content = ({ parts }) => {
+    return (
+      <div>
+        {parts.map((part, index) => (
+          <Part key={index} part={part} />
+        ))}
+      </div>
+    );
+  };
+
+  return (
+    <div>
+      <Header course={course} />
+      <Content parts={course.parts} />
+    </div>
+  );
+};
+
+const App = () => {
+  const course = {
+    name: "Half Stack application development",
+    parts: [
+      {
+        name: "Fundamentals of React",
+        exercises: 10,
+      },
+      {
+        name: "Using props to pass data",
+        exercises: 7,
+      },
+      {
+        name: "State of a component",
+        exercises: 14,
+      },
+
+      {
+        name: "Redux",
+        exercises: 11,
+      },
+    ],
+  };
+
+  return (
+    <div>
+      <Course course={course} />
+    </div>
+  );
+};
+
+export default App;
