e53c1cf141e31b9750766b16684e39fbaefd347e
jasminlei <jasminleinonen@hotmail.com>
2024-12-27 21:29:15 +0200

    teht 2.8

--- osa2/puhelinluettelo/src/App.jsx
+++ osa2/puhelinluettelo/src/App.jsx
@@ -1,10 +1,12 @@
 import { useState } from "react";
 
-const Names = ({ persons }) => {
+const NamesAndNumbers = ({ persons }) => {
   return (
     <div>
       {persons.map((person) => (
-        <p key={person.name}>{person.name}</p>
+        <p key={person.name}>
+          {person.name} {person.number}
+        </p>
       ))}
     </div>
   );
@@ -13,10 +15,11 @@ const Names = ({ persons }) => {
 const App = () => {
   const [persons, setPersons] = useState([{ name: "Arto Hellas" }]);
   const [newName, setNewName] = useState("");
+  const [newNumber, setNewNumber] = useState("");
 
   const addNewPerson = (event) => {
     event.preventDefault();
-    const newPerson = { name: newName };
+    const newPerson = { name: newName, number: newNumber };
 
     const nameAlreadyUsed = persons.some((person) => person.name === newName);
 
@@ -25,11 +28,16 @@ const App = () => {
     } else {
       setPersons(persons.concat(newPerson));
       setNewName("");
+      setNewNumber("");
     }
   };
 
   const handleNameChange = (event) => {
     setNewName(event.target.value);
+  };
+
+  const handleNumberChange = (event) => {
+    setNewNumber(event.target.value);
   };
 
   return (
@@ -40,11 +48,14 @@ const App = () => {
           name: <input value={newName} onChange={handleNameChange} />
         </div>
         <div>
+          number: <input value={newNumber} onChange={handleNumberChange} />
+        </div>
+        <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
-      <Names persons={persons} />
+      <NamesAndNumbers persons={persons} />
     </div>
   );
 };
