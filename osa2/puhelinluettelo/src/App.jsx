11baa869f7c15164cd7675d1df9d8094c99d9d63
jasminlei <jasminleinonen@hotmail.com>
2024-12-27 20:17:26 +0200

    teht 2.6

---
+++ osa2/puhelinluettelo/src/App.jsx
@@ -0,0 +1,45 @@
+import { useState } from "react";
+
+const Names = ({ persons }) => {
+  return (
+    <div>
+      {persons.map((person) => (
+        <p key={person.name}>{person.name}</p>
+      ))}
+    </div>
+  );
+};
+
+const App = () => {
+  const [persons, setPersons] = useState([{ name: "Arto Hellas" }]);
+  const [newName, setNewName] = useState("");
+
+  const addNewPerson = (event) => {
+    event.preventDefault();
+    const newPerson = { name: newName };
+    setPersons(persons.concat(newPerson));
+    setNewName("");
+  };
+
+  const handleNameChange = (event) => {
+    setNewName(event.target.value);
+  };
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <form onSubmit={addNewPerson}>
+        <div>
+          name: <input value={newName} onChange={handleNameChange} />
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      <Names persons={persons} />
+    </div>
+  );
+};
+
+export default App;
