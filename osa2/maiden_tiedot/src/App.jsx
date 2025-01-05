28fa8720697acec82165616c747d8c7a5212addf
jasminlei <jasminleinonen@hotmail.com>
2025-01-05 17:06:40 +0200

    teht 2.19

--- osa2/maiden_tiedot/src/App.jsx
+++ osa2/maiden_tiedot/src/App.jsx
@@ -9,39 +9,58 @@ const Search = ({ searchQuery, handleSearchChange }) => {
   );
 };
 
-const Content = ({ countries }) => {
+const Country = ({ country }) => {
+  return (
+    <div>
+      <h1>{country.name.common}</h1>
+      <p>Capital: {country.capital}</p>
+      <p>Area: {country.area}</p>
+      <p>Languages:</p>
+      <ul>
+        {Object.values(country.languages).map((language, index) => (
+          <li key={index}>{language}</li>
+        ))}
+      </ul>
+      <img src={country.flags.png} alt={`Flag of ${country.name.common}`} />
+    </div>
+  );
+};
+
+const Content = ({ countries, searchQuery }) => {
+  const [selectedCountry, setSelectedCountry] = useState(null);
+
+  useEffect(() => {
+    setSelectedCountry(null);
+  }, [searchQuery]);
+
+  const showCountry = (country) => {
+    setSelectedCountry(country);
+  };
+
+  if (selectedCountry) {
+    return (
+      <div>
+        <Country country={selectedCountry} />
+        <button onClick={() => setSelectedCountry(null)}>back</button>
+      </div>
+    );
+  }
+
   if (countries.length < 11 && countries.length > 1) {
     return (
       <div>
         {countries.map((country, index) => (
-          <div key={index}>{country.name.common}</div>
+          <div key={index}>
+            {country.name.common}{" "}
+            <button onClick={() => showCountry(country)}>show</button>
+          </div>
         ))}
       </div>
     );
   } else if (countries.length > 10) {
     return <div>Too many matches, spesify another filter</div>;
-  } else {
-    return (
-      <div>
-        {countries.map((country, index) => (
-          <div key={index}>
-            <h1>{country.name.common}</h1>
-            <p>Capital: {country.capital}</p>
-            <p>Area: {country.area}</p>
-            <p>Languages:</p>
-            <ul>
-              {Object.values(country.languages).map((language, index) => (
-                <li key={index}>{language}</li>
-              ))}
-            </ul>
-            <img
-              src={country.flags.png}
-              alt={`Flag of ${country.name.common}`}
-            />
-          </div>
-        ))}
-      </div>
-    );
+  } else if (countries.length === 1) {
+    return <Country country={countries[0]} />;
   }
 };
 
@@ -77,7 +96,7 @@ const App = () => {
         searchQuery={searchQuery}
         handleSearchChange={handleSearchChange}
       />
-      <Content countries={countries} />
+      <Content countries={countries} searchQuery={searchQuery} />
     </div>
   );
 };
