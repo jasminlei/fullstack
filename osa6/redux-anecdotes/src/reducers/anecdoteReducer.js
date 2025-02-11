8242932558a7b1657dfa9201b8faf65eb1fae0f6
jasminlei <jasminleinonen@hotmail.com>
2025-02-11 18:53:15 +0200

    teht 6.10-11

--- osa6/redux-anecdotes/src/reducers/anecdoteReducer.js
+++ osa6/redux-anecdotes/src/reducers/anecdoteReducer.js
@@ -1,3 +1,5 @@
+import { createSlice } from '@reduxjs/toolkit'
+
 const anecdotesAtStart = [
   'If it hurts, do it more often',
   'Adding manpower to a late software project makes it later!',
@@ -19,45 +21,25 @@ const asObject = (anecdote) => {
 
 const initialState = anecdotesAtStart.map(asObject)
 
-const anecdoteReducer = (state = initialState, action) => {
-  console.log('state now: ', state)
-  console.log('action', action)
-
-  switch (action.type) {
-    case 'NEW_ANECDOTE':
-      return [...state, action.payload]
-
-    case 'VOTE': {
-      const id = action.payload.id
-      const updatedState = state.map((anecdote) =>
-        anecdote.id !== id
-          ? anecdote
-          : { ...anecdote, votes: anecdote.votes + 1 }
-      )
-      return updatedState.sort((a, b) => b.votes - a.votes)
-    }
-
-    default:
-      return state
-  }
-}
-
-export const createAnecdote = (content) => {
-  return {
-    type: 'NEW_ANECDOTE',
-    payload: {
-      content,
-      votes: 0,
-      id: getId(),
+const anecdoteSlice = createSlice({
+  name: 'anecdotes',
+  initialState,
+  reducers: {
+    createAnecdote(state, action) {
+      const content = action.payload
+      state.push(asObject(content))
     },
-  }
-}
+    voteAnecdote(state, action) {
+      const id = action.payload
+      const anecdote = state.find((a) => a.id === id)
+      if (anecdote) {
+        anecdote.votes += 1
+      }
 
-export const voteAnecdote = (id) => {
-  return {
-    type: 'VOTE',
-    payload: { id },
-  }
-}
+      state.sort((a, b) => b.votes - a.votes)
+    },
+  },
+})
 
-export default anecdoteReducer
+export const { createAnecdote, voteAnecdote } = anecdoteSlice.actions
+export default anecdoteSlice.reducer
--- osa6/redux-anecdotes/src/reducers/filterReducer.js
+++ osa6/redux-anecdotes/src/reducers/filterReducer.js
@@ -1,20 +1,14 @@
-const filterReducer = (state = '', action) => {
-  console.log('state now: ', state)
-  console.log('action', action)
+import { createSlice } from '@reduxjs/toolkit'
 
-  switch (action.type) {
-    case 'CHANGE_FILTER':
+const filterSlice = createSlice({
+  name: 'filter',
+  initialState: '',
+  reducers: {
+    setFilter(state, action) {
       return action.payload
-    default:
-      return state
-  }
-}
+    },
+  },
+})
 
-export const setFilter = (content) => {
-  return {
-    type: 'CHANGE_FILTER',
-    payload: content,
-  }
-}
-
-export default filterReducer
+export const { setFilter } = filterSlice.actions
+export default filterSlice.reducer
